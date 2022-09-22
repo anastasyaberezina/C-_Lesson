c455a9c90652992e78044f03dae48a1c01d4747a
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-23 01:30:44 +0300

    массив

---
+++ Example_009_massiv/Program.cs
@@ -0,0 +1,22 @@
+﻿//Нахождение максимума из 9 чисел обычным методом ("стихийный способ").
+int Max (int arg1, int arg2, int arg3 )
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+//               0   1  2  3   4   5    6   7  8
+int[] array = { 11, 92, 3, 45, 57, 36, 72, 18, 92 };
+
+//array[0] = 12;
+//Console.WriteLine(array[4])
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
+
