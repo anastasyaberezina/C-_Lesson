ba8d72dddc6ddddb2d4df5525ff324367e824910
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-23 01:31:45 +0300

    массив с while

---
+++ Example_010_array/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1, 52, 95, 26, 4, 52, 14 };
+
+int n = array.Length;
+int find = 52;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+//index = index + 1;
+index++;
+}
